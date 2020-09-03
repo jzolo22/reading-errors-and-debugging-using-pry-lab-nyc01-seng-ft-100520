5466be36f868d0fbd885a0ee7278adca0ad59b8f
Julia Zolotarev <jzolotarev@gmail.com>
2020-09-03 14:28:28 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -4,9 +4,9 @@ require "pry"
 def snake_it_up(string)
   if string[0] == "s"
     10.times do
-      print "s"
+      p "s"
     end
-      puts string
+      p string
   else
      puts "#{string}"
    end
