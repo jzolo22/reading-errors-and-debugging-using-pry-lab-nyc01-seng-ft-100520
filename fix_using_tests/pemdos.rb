1aaadcc93faa86c3693c596b87de35a81b11d9ec
Julia Zolotarev <jzolotarev@gmail.com>
2020-09-03 14:51:45 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,20 +1,20 @@
 # don't forget to add: require 'pry'
 require "pry"
 
-def snake_it_up(string)
-  if string[0] == "s"
-    10.times do
-      print "s"
-    end
-      p string
-      binding.pry
-  else
-     p "#{string}"
-     binding.pry
-   end
-end
+# def snake_it_up(string)
+#   if string[0] == "s"
+#     10.times do
+#       print "s"
+#     end
+#       p string
+#       binding.pry
+#   else
+#     p "#{string}"
+#     binding.pry
+#   end
+# end
 
-snake_it_up("surprise!")
+
 
 def snake_it_up(string)
   if string[0] == "s"
